e16b12095184e6e2bc9c3b29dcd53d1f0d385b6f
Koushik Dutta <koushd@gmail.com>
2018-12-19 13:26:13 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('test');
+
+function VirtualDevice() {
+}
+
+// implementation of OnOff
+
+VirtualDevice.prototype.getOnOff = function() {
+  console.log('getOnOff was called!');
+}
+
+VirtualDevice.prototype.setOnOff = function(arg0) {
+  console.log('setOnOff was called!');
+}
+
+
+return new VirtualDevice()
