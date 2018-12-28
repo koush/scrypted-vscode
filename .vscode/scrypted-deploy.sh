28c205ca5d1f23893d1f1166ceec6fcc57d8853e
Koushik Dutta <koushd@gmail.com>
2018-12-27 16:51:03 -0800

    simplify setup

--- .vscode/scrypted-deploy.sh
+++ .vscode/scrypted-deploy.sh
@@ -5,43 +5,39 @@ then
     echo "scrypted.debugHost not defined in workspace settings"
     exit 1
 fi
+debugHost=$1
 
 if [ -z "$2" ]
 then
-    echo "scrypted.debugScriptId not defined in workspace settings"
+    echo "no deploy script provided"
     exit 2
 fi
 
-if [ -z "$3" ]
-then
-    echo "no deploy script provided"
-    exit 3
-fi
-
-filename=$3
+filename=$2
 extension="${filename##*.}"
 
-if [ -z "$4" ]
+if [ -z "$3" ]
 then
     echo "outDir paramter missing. Please ensure it is provided in an argument in tasks.json and has a matching value in launch.json."
-    exit 4
+    exit 3
 fi
-outdir=$4
+outdir=$3
 
 # all files must be transpiled since duktape is ecmascript 5. find the transpiled file.
 filenameNoExtension=$(basename "${filename%.*}")
 javascriptFilename="$outdir/$filenameNoExtension.js"
 if [ ! -f "$javascriptFilename" ]
 then
-    echo "Transpiled TypeScript file not found: $javascriptFilename"
-    exit 41
+    # echo "Transpiled TypeScript file not found: $javascriptFilename"
+    # exit 41
+    javascriptFilename=$filename
 fi
 echo $javascriptFilename
 filename=$javascriptFilename
 
 strippedFilename=$(basename $filename)
 
-DEBUG_URL="https://$1:9443/component/script/debug?scriptId=$2&filename=$strippedFilename&engine=duktape"
+DEBUG_URL="https://$debugHost:9443/component/script/debug?filename=$strippedFilename&engine=duktape"
 echo "deploying to $DEBUG_URL"
 
 # dump the contents to stderr for debugging, but observe the status code in stdout
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,23 @@ var webpack = require('webpack');
 var outputFilename = process.env.TARGET ? path.basename(process.env.TARGET.split('.').slice(0, -1).join('.')) + '.js' : null;
 var out = path.resolve(__dirname, 'out');
 
+var polyfillPlugins = [];
+var entry = [];
+
+// change this value to enable or disable babel polyfill. using babel will grealy increase the size of the script.
+// typescript is recommended for this reason.
+var useBabel = false;
+if (useBabel) {
+    polyfillPlugins.push(new webpack.ProvidePlugin({
+        'babel-polyfill': 'global.Promise'
+    }));
+    entry.push('babel-polyfill');
+}
+
+entry.push(process.env.TARGET);
+
 module.exports = {
+    entry: entry,
     output: {
         path: out,
         filename: outputFilename,
@@ -34,18 +50,15 @@ module.exports = {
         ]
     },
 
-    plugins: [
-        new webpack.ProvidePlugin({
-            'babel-polyfill': 'global.Promise'
-        })
-    ],
+    plugins: polyfillPlugins,
 
 
     stats: {
         colors: true
     },
     optimization: {
-        minimize: false
+        // can not minimize since duktape only does line based breakpoints
+        minimize: false,
     },
     devtool: 'nosources-source-map'
 };
